4d877836ed7d57d377bb989697f0f15f9f366795
Tim <tim@xcel.digital>
2020-08-26 01:24:14 -0700

    Restructure folders

---
+++ src/shared/orbSkins.ts
@@ -0,0 +1,52 @@
+export const ORB_SKINS = {
+	0: 'Normal Orbs', // ?
+	1: 'Fancy Orbs', // ?
+	2: 'Valkyrie Orbs',
+	3: 'Zeus Orbs',
+	4: 'Athena Orbs',
+	5: 'Kamen Rider Orbs',
+	6: 'Yokai Watch World Orbs', // ?
+	7: 'Shinrabansho Choco Orbs',
+	8: 'Greco-Roman Gods Orbs',
+	9: 'SFV: AE Orbs',
+	10: 'Mystic Knight Orbs',
+	11: 'Healer Girls Orbs',
+	12: 'Hera Orbs',
+	13: 'Yu Yu Hakusho Orbs',
+	14: 'Monster Hunter Orbs',
+	15: 'Japanese Gods Orbs',
+	16: 'Shaman King Orbs',
+	17: 'SANRIO CHARACTERS Orbs',
+	18: '*****(Attack on Titan) Orbs', // ?
+	19: 'Indian Gods Orbs',
+	20: 'FINAL FANTASY Orbs',
+	21: '*****(champion) Orbs', // ? Shonen Champion Collab
+	22: 'Norse Gods Orbs',
+	23: '***** (Duel Masters)', // ?
+	24: '*****(power pros)', // ?
+	25: '*****(magazine)', // ?
+	26: 'Samurai Shodown Orbs',
+	27: 'Egyptian Gods Orbs',
+	28: 'Five Elements Orbs',
+	29: 'Dragon Kings Orbs',
+	30: 'Fantasia Legend Orbs',
+	31: '***** (Shin)', // ?
+	32: 'Kamen Rider 2 Orbs',
+	33: 'Angels Orbs',
+	34: 'Yu-Gi-Oh! DM Orbs',
+	35: 'Devils Orbs',
+	36: 'SAO Orbs',
+	37: 'Hero Gods Orbs',
+	38: 'Ninjas Orbs',
+	39: 'Golems Orbs',
+	40: 'Animals Orbs',
+	41: '*****(Mickey)', // ?
+	42: 'Gintama Orbs',
+	43: 'BLEACH Orbs',
+	44: 'Greco-Roman 2 Gods Orbs',
+	45: 'DMC Orbs',
+	46: 'Pixel Orbs',
+	47: 'Japanese 2 Gods Orbs',
+	48: 'Draconic Orchestra Orbs',
+	49: 'Evangelion Orbs',
+};
